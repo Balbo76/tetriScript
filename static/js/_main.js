74f243c712b499c8f1a9363a42e6f65de3424d7b
balboalessandro@gmail.com <balboalessandro@gmail.com>
2018-05-03 19:22:02 +0200

    moving to pixi.js

---
+++ static/js/_main.js
@@ -0,0 +1,400 @@
+
+var
+    canvas = document.getElementById("tetrisCanvas"),
+    context = canvas.getContext("2d"),
+    canvasHeight = canvas.height,
+    canvasWidth = canvas.width,
+    blockSize = 22,
+    offsetX = 110,
+    offsetY = 40,
+    tetramini = [
+        [
+            [
+                [0, 0, 0, 0],
+                [0, 0, 0, 0],
+                [1, 1, 1, 1],
+                [0, 0, 0, 0]
+            ],
+            [
+                [0,1, 0, 0],
+                [0,1, 0, 0],
+                [0,1, 0, 0],
+                [0,1, 0, 0]
+            ],
+            [
+                [0, 0, 0, 0],
+                [1, 1, 1, 1],
+                [0, 0, 0, 0],
+                [0, 0, 0, 0]
+            ],
+            [
+                [0,1, 0, 0],
+                [0,1, 0, 0],
+                [0,1, 0, 0],
+                [0,1, 0, 0]
+            ]
+        ],
+
+        [
+            [
+                [0, 0, 0, 0],
+                [1, 0, 0, 0],
+                [1, 1, 1, 0],
+                [0, 0, 0, 0]
+            ],
+            [
+                [0,1, 1, 0],
+                [0,1, 0, 0],
+                [0,1, 0, 0],
+                [0,0, 0, 0]
+            ],
+            [
+                [0, 0, 0, 0],
+                [1, 1, 1, 0],
+                [0, 0, 1, 0],
+                [0, 0, 0, 0]
+            ],
+            [
+                [0,0, 1, 0],
+                [0,0, 1, 0],
+                [0,1, 1, 0],
+                [0,0, 0, 0]
+            ]
+        ],
+
+        [
+            [
+                [0, 0, 0, 0],
+                [0, 0, 1, 0],
+                [1, 1, 1, 0],
+                [0, 0, 0, 0]
+            ],
+            [
+                [0,1, 0, 0],
+                [0,1, 0, 0],
+                [0,1, 1, 0],
+                [0,0, 0, 0]
+            ],
+            [
+                [0, 0, 0, 0],
+                [1, 1, 1, 0],
+                [1, 0, 0, 0],
+                [0, 0, 0, 0]
+            ],
+            [
+                [0,1, 1, 0],
+                [0,0, 1, 0],
+                [0,0, 1, 0],
+                [0,0, 0, 0]
+            ]
+        ],
+
+
+        [
+            [
+                [0, 0, 0, 0],
+                [0, 1, 1, 0],
+                [0, 1, 1, 0],
+                [0, 0, 0, 0]
+            ],
+            [
+                [0, 0, 0, 0],
+                [0, 1, 1, 0],
+                [0, 1, 1, 0],
+                [0, 0, 0, 0]
+            ],
+            [
+                [0, 0, 0, 0],
+                [0, 1, 1, 0],
+                [0, 1, 1, 0],
+                [0, 0, 0, 0]
+            ],
+            [
+                [0, 0, 0, 0],
+                [0, 1, 1, 0],
+                [0, 1, 1, 0],
+                [0, 0, 0, 0]
+            ]
+        ],
+
+        [
+            [
+                [0, 0, 0, 0],
+                [0, 1, 1, 0],
+                [1, 1, 0, 0],
+                [0, 0, 0, 0]
+            ],
+            [
+                [0, 0, 0, 0],
+                [0, 1, 0, 0],
+                [0, 1, 1, 0],
+                [0, 0, 1, 0]
+            ],
+            [
+                [0, 0, 0, 0],
+                [0, 1, 1, 0],
+                [1, 1, 0, 0],
+                [0, 0, 0, 0]
+            ],
+            [
+                [0, 0, 0, 0],
+                [0, 1, 0, 0],
+                [0, 1, 1, 0],
+                [0, 0, 1, 0]
+            ]
+        ],
+
+        [
+            [
+                [0, 0, 0, 0],
+                [0, 1, 0, 0],
+                [1, 1, 1, 0],
+                [0, 0, 0, 0]
+            ],
+            [
+                [0, 0, 0, 0],
+                [0, 1, 0, 0],
+                [0, 1, 1, 0],
+                [0, 1, 0, 0]
+            ],
+            [
+                [0, 0, 0, 0],
+                [0, 0, 0, 0],
+                [1, 1, 1, 0],
+                [0, 1, 0, 0]
+            ],
+            [
+                [0, 0, 0, 0],
+                [0, 1, 0, 0],
+                [1, 1, 0, 0],
+                [0, 1, 0, 0]
+            ]
+        ],
+
+        [
+            [
+                [0, 0, 0, 0],
+                [1, 1, 0, 0],
+                [0, 1, 1, 0],
+                [0, 0, 0, 0]
+            ],
+            [
+                [0, 0, 0, 0],
+                [0, 0, 1, 0],
+                [0, 1, 1, 0],
+                [0, 1, 0, 0]
+            ],
+            [
+                [0, 0, 0, 0],
+                [1, 1, 0, 0],
+                [0, 1, 1, 0],
+                [0, 0, 0, 0]
+            ],
+            [
+                [0, 0, 0, 0],
+                [0, 0, 1, 0],
+                [0, 1, 1, 0],
+                [0, 1, 0, 0]
+            ]
+        ],
+    ],
+
+    System = function() {
+        var that = this;
+        this.output = new Screen();
+        this.input = function(event) {
+            if (that.output.gameState == "intro") {
+                if(event.keyCode == 32) {
+                    room.send({start: true});
+                    that.output.gameState = "partita";
+                }
+            } else if (that.output.gameState == "partita") {
+                if(event.keyCode == 37) {
+                    room.send({ dir: "left"});
+
+                }
+                if(event.keyCode == 39) {
+                    room.send({ dir: "right"});
+
+                }
+                if(event.keyCode == 40) {
+                    room.send({ dir: "down"});
+
+                }
+                if(event.keyCode == 32) {
+                    room.send({ dir: "rotate"});
+
+                }
+            } else if (that.output.gameState == "gameover") {
+                if(event.keyCode == 32) {
+                    that.output.gameState = "intro";
+                }
+            }
+        };
+    },
+
+    Screen = function() {
+        this.partita = false;
+        this.gameState = "intro";
+        this.processFrame = function() {
+            if (this.gameState == "intro") { this.drawTitle(); }
+            if (this.gameState == "partita") {  this.drawFrame();  }
+            if (this.gameState == "gameover") { this.drawGameOver(); }
+        };
+        this.clearScreen = function() {
+            context.fillStyle = "black";
+            context.fillRect(0,  0,  canvasWidth, canvasHeight);
+        };
+        this.drawTitle = function() {
+            this.clearScreen();
+            context.font = "140px Times";
+            context.fillStyle = "white";
+            context.fillText("Tetris", 80,  350);
+        };
+        this.drawGameOver = function() {
+            // this.clearScreen();
+            context.font = "130px Times";
+            context.fillStyle = "red";
+            context.fillText("Game Over", 100,  400);
+        };
+        this.setTetraminoFillStyle = function(i) {
+            if (i == 1) { context.fillStyle = "#b3ffff"; }
+            if (i == 2) { context.fillStyle = "#0073e6"; }
+            if (i == 3) { context.fillStyle = "#ff9933"; }
+            if (i == 4) { context.fillStyle = "#ffff00"; }
+            if (i == 5) { context.fillStyle = "#66ff33"; }
+            if (i == 6) { context.fillStyle = "#ff66ff"; }
+            if (i == 7) { context.fillStyle = "#cc0000"; }
+        };
+        this.drawFrameTest = function(partita){
+            this.clearScreen();
+            for (var i=0; i<=24; i++){
+                for (var j=0; j<=11; j++){
+                    var x = j * blockSize, y = i * blockSize;
+
+                    if (partita.schermata[i][j] == 0) {
+                        context.fillStyle = "white";
+                        context.fillRect(offsetX + x, offsetY + y, blockSize - 1, blockSize - 1);
+                    }
+
+                    if (partita.schermata[i][j] > 0) {
+                        this.setTetraminoFillStyle(partita.schermata[i][j]);
+                        if (partita.schermata[i][j] == 255) { context.fillStyle = "#FF0000"; }
+                        context.fillRect(offsetX + x, offsetY + y, blockSize - 1, blockSize - 1);
+                    }
+                }
+            }
+
+            // Draw current
+            for (var i=0; i<=3; i++) {
+                for (var j=0; j<=3; j++) {
+                    if (tetramini[partita.tetramino.corrente][partita.tetramino.rotazione][i][j] == 1) {
+                        var
+                            x = offsetX + ((partita.tetramino.x + j) * blockSize),
+                            y = offsetY + ((partita.tetramino.y + i) * blockSize);
+                        this.setTetraminoFillStyle(partita.tetramino.corrente + 1);
+                        context.fillRect(x,  y, blockSize - 1, blockSize - 1);
+                    }
+                }
+            }
+        };
+        this.drawFrame = function(){
+
+            var partita = this.partita;
+
+            this.clearScreen();
+
+            context.strokeStyle = "white";
+            context.beginPath();
+            context.moveTo(offsetX + 13, offsetY - 5);
+            context.lineTo(offsetX + 13, offsetY + blockSize * 24 + 10);
+            context.moveTo(offsetX + 5 , offsetY + blockSize * 24 + 5);
+            context.lineTo(offsetX + 10 + 11 * blockSize, offsetY + blockSize * 24 + 5);
+            context.moveTo(offsetX + 5 + 11 * blockSize, offsetY - 5);
+            context.lineTo(offsetX + 5 + 11 * blockSize, offsetY + blockSize * 24 + 10);
+            context.stroke();
+
+            if (partita.schermata) {
+
+
+                for (var i = 0; i < 24; i++) {
+                    for (var j = 1; j < 11; j++) {
+                        var x = j * blockSize, y = i * blockSize;
+
+                        if (partita.schermata[i][j] == 0) {
+                            context.fillStyle = "white";
+                            //context.fillRect(offsetX + x, offsetY + y, blockSize - 1, blockSize - 1);
+                        }
+
+                        if (partita.schermata[i][j] > 0) {
+                            this.setTetraminoFillStyle(partita.schermata[i][j]);
+                            if (partita.schermata[i][j] == 255) {
+                                context.fillStyle = "#FF0000";
+                            }
+                            context.fillRect(offsetX + x, offsetY + y, blockSize - 1, blockSize - 1);
+                        }
+                    }
+                }
+
+                // Draw current
+                for (var i = 0; i <= 3; i++) {
+                    for (var j = 0; j <= 3; j++) {
+                        if (tetramini[partita.tetramino.corrente][partita.tetramino.rotazione][i][j] == 1) {
+                            var
+                                x = offsetX + ((partita.tetramino.x + j) * blockSize),
+                                y = offsetY + ((partita.tetramino.y + i) * blockSize);
+                            this.setTetraminoFillStyle(partita.tetramino.corrente + 1);
+                            context.fillRect(x, y, blockSize - 1, blockSize - 1);
+                        }
+                    }
+                }
+
+            }
+
+        };
+        this.setPartita = function(partita){
+            this.partita = partita;
+        };
+    },
+    system = new System(),
+    host = window.document.location.host.replace(/:.*/, ''),
+    client = new Colyseus.Client(location.protocol.replace("http", "ws") + host + (location.port ? ':'+location.port : '')),
+    room = client.join("tetris");
+
+document.addEventListener('keydown', system.input); // Motore Input
+setInterval(function(){ system.output.processFrame(); }, 40);	// Motore grafico
+
+
+room.onJoin.add(function () {
+    console.log("joined");
+
+});
+
+room.onStateChange.addOnce(function(state) { });
+// new room state
+room.onStateChange.add(function(state) { });
+// listen to patches coming from the server
+room.onMessage.add(function(data) { });
+
+
+room.listen("players/:uid", function(change)  {
+    if (change.operation == "add"){
+        system.output.partita = change.value;
+    }
+});
+room.listen("players/:uid/gameOver", function(change)  {
+    if ((change.operation == "replace") && (change.value == true)){
+        room.send({stop: true});
+        system.output.gameState = "gameover";
+    }
+});
+room.listen("players/:uid/schermata/:i/:j", function(change)  {
+    if (change.operation == "replace"){
+        system.output.partita.schermata[change.path["i"]][change.path["j"]] = change.value;
+    }
+});
+room.listen("players/:uid:/tetramino/:t", function(change)  {
+    if (change.operation == "replace"){
+        system.output.partita.tetramino[change.path["t"]] = change.value;
+    }
+});
--- static/js/main.js
+++ static/js/main.js
@@ -1,9 +1,4 @@
-
-var
-    canvas = document.getElementById("tetrisCanvas"),
-    context = canvas.getContext("2d"),
-    canvasHeight = canvas.height,
-    canvasWidth = canvas.width,
+const
     blockSize = 22,
     offsetX = 110,
     offsetY = 40,
@@ -197,172 +192,123 @@ var
                 [0, 1, 0, 0]
             ]
         ],
-    ],
-
-    System = function() {
-        var that = this;
-        this.output = new Screen();
-        this.input = function(event) {
-            if (that.output.gameState == "intro") {
-                if(event.keyCode == 32) {
-                    room.send({start: true});
-                    that.output.gameState = "partita";
-                }
-            } else if (that.output.gameState == "partita") {
-                if(event.keyCode == 37) {
-                    room.send({ dir: "left"});
-
-                }
-                if(event.keyCode == 39) {
-                    room.send({ dir: "right"});
-
-                }
-                if(event.keyCode == 40) {
-                    room.send({ dir: "down"});
-
-                }
-                if(event.keyCode == 32) {
-                    room.send({ dir: "rotate"});
-
-                }
-            } else if (that.output.gameState == "gameover") {
-                if(event.keyCode == 32) {
-                    that.output.gameState = "intro";
-                }
-            }
-        };
-    },
-
-    Screen = function() {
-        this.partita = false;
-        this.gameState = "intro";
-        this.processFrame = function() {
-            if (this.gameState == "intro") { this.drawTitle(); }
-            if (this.gameState == "partita") {  this.drawFrame();  }
-            if (this.gameState == "gameover") { this.drawGameOver(); }
-        };
-        this.clearScreen = function() {
-            context.fillStyle = "black";
-            context.fillRect(0,  0,  canvasWidth, canvasHeight);
-        };
-        this.drawTitle = function() {
-            this.clearScreen();
-            context.font = "140px Times";
-            context.fillStyle = "white";
-            context.fillText("Tetris", 80,  350);
-        };
-        this.drawGameOver = function() {
-            // this.clearScreen();
-            context.font = "130px Times";
-            context.fillStyle = "red";
-            context.fillText("Game Over", 100,  400);
-        };
-        this.setTetraminoFillStyle = function(i) {
-            if (i == 1) { context.fillStyle = "#b3ffff"; }
-            if (i == 2) { context.fillStyle = "#0073e6"; }
-            if (i == 3) { context.fillStyle = "#ff9933"; }
-            if (i == 4) { context.fillStyle = "#ffff00"; }
-            if (i == 5) { context.fillStyle = "#66ff33"; }
-            if (i == 6) { context.fillStyle = "#ff66ff"; }
-            if (i == 7) { context.fillStyle = "#cc0000"; }
-        };
-        this.drawFrameTest = function(partita){
-            this.clearScreen();
-            for (var i=0; i<=24; i++){
-                for (var j=0; j<=11; j++){
-                    var x = j * blockSize, y = i * blockSize;
-
-                    if (partita.schermata[i][j] == 0) {
-                        context.fillStyle = "white";
-                        context.fillRect(offsetX + x, offsetY + y, blockSize - 1, blockSize - 1);
-                    }
-
-                    if (partita.schermata[i][j] > 0) {
-                        this.setTetraminoFillStyle(partita.schermata[i][j]);
-                        if (partita.schermata[i][j] == 255) { context.fillStyle = "#FF0000"; }
-                        context.fillRect(offsetX + x, offsetY + y, blockSize - 1, blockSize - 1);
-                    }
-                }
-            }
-
-            // Draw current
-            for (var i=0; i<=3; i++) {
-                for (var j=0; j<=3; j++) {
-                    if (tetramini[partita.tetramino.corrente][partita.tetramino.rotazione][i][j] == 1) {
-                        var
-                            x = offsetX + ((partita.tetramino.x + j) * blockSize),
-                            y = offsetY + ((partita.tetramino.y + i) * blockSize);
-                        this.setTetraminoFillStyle(partita.tetramino.corrente + 1);
-                        context.fillRect(x,  y, blockSize - 1, blockSize - 1);
-                    }
-                }
-            }
-        };
-        this.drawFrame = function(){
-
-            var partita = this.partita;
-
-            this.clearScreen();
-
-            context.strokeStyle = "white";
-            context.beginPath();
-            context.moveTo(offsetX + 13, offsetY - 5);
-            context.lineTo(offsetX + 13, offsetY + blockSize * 24 + 10);
-            context.moveTo(offsetX + 5 , offsetY + blockSize * 24 + 5);
-            context.lineTo(offsetX + 10 + 11 * blockSize, offsetY + blockSize * 24 + 5);
-            context.moveTo(offsetX + 5 + 11 * blockSize, offsetY - 5);
-            context.lineTo(offsetX + 5 + 11 * blockSize, offsetY + blockSize * 24 + 10);
-            context.stroke();
-
-            if (partita.schermata) {
+    ];
 
 
-                for (var i = 0; i < 24; i++) {
-                    for (var j = 1; j < 11; j++) {
-                        var x = j * blockSize, y = i * blockSize;
 
-                        if (partita.schermata[i][j] == 0) {
-                            context.fillStyle = "white";
-                            //context.fillRect(offsetX + x, offsetY + y, blockSize - 1, blockSize - 1);
-                        }
+let app = new PIXI.Application(800, 600, {backgroundColor : 0x1099bb});
+document.body.appendChild(app.view);
 
-                        if (partita.schermata[i][j] > 0) {
-                            this.setTetraminoFillStyle(partita.schermata[i][j]);
-                            if (partita.schermata[i][j] == 255) {
-                                context.fillStyle = "#FF0000";
-                            }
-                            context.fillRect(offsetX + x, offsetY + y, blockSize - 1, blockSize - 1);
-                        }
-                    }
-                }
 
-                // Draw current
-                for (var i = 0; i <= 3; i++) {
-                    for (var j = 0; j <= 3; j++) {
-                        if (tetramini[partita.tetramino.corrente][partita.tetramino.rotazione][i][j] == 1) {
-                            var
-                                x = offsetX + ((partita.tetramino.x + j) * blockSize),
-                                y = offsetY + ((partita.tetramino.y + i) * blockSize);
-                            this.setTetraminoFillStyle(partita.tetramino.corrente + 1);
-                            context.fillRect(x, y, blockSize - 1, blockSize - 1);
-                        }
-                    }
-                }
 
-            }
 
-        };
-        this.setPartita = function(partita){
-            this.partita = partita;
-        };
-    },
-    system = new System(),
-    host = window.document.location.host.replace(/:.*/, ''),
+
+var bunny = PIXI.Sprite.fromImage('img/star.png')
+
+// center the sprite's anchor point
+bunny.anchor.set(0.5);
+
+// move the sprite to the center of the screen
+bunny.x = app.screen.width / 2;
+bunny.y = app.screen.height / 2;
+
+app.stage.addChild(bunny);
+
+
+
+
+
+
+
+let
+    left = keyboard(37),
+    up = keyboard(38),
+    right = keyboard(39),
+    down = keyboard(40),
+    space = keyboard(32);
+
+left.press = () => { console.log('left press'); };
+left.release = () => { console.log('left release');  };
+
+up.press = () => { console.log('up press'); };
+up.release = () => { };
+
+right.press = () => { console.log('right press'); };
+right.release = () => { };
+
+down.press = () => { console.log('down press'); };
+down.release = () => { };
+
+space.press = () => { console.log('space press'); };
+space.release = () => { };
+
+
+function keyboard(keyCode) {
+    let key = {};
+    key.code = keyCode;
+    key.isDown = false;
+    key.isUp = true;
+    key.press = undefined;
+    key.release = undefined;
+    //The `downHandler`
+    key.downHandler = event => {
+        if (event.keyCode === key.code) {
+            if (key.isUp && key.press) key.press();
+            key.isDown = true;
+            key.isUp = false;
+        }
+        event.preventDefault();
+    };
+
+    //The `upHandler`
+    key.upHandler = event => {
+        if (event.keyCode === key.code) {
+            if (key.isDown && key.release) key.release();
+            key.isDown = false;
+            key.isUp = true;
+        }
+        event.preventDefault();
+    };
+
+    //Attach event listeners
+    window.addEventListener(
+        "keydown", key.downHandler.bind(key), false
+    );
+    window.addEventListener(
+        "keyup", key.upHandler.bind(key), false
+    );
+    return key;
+}
+
+
+
+
+
+
+
+
+state = play;
+// Listen for animate update
+app.ticker.add(delta => gameLoop(delta));
+function gameLoop(delta) {
+    state(delta);
+
+
+}
+
+function play(delta) {
+    // just for fun, let's rotate mr rabbit a little
+    // delta is 1 if running at 100% performance
+    // creates frame-independent transformation
+    bunny.rotation += 0.1 * delta;
+}
+
+
+let host = window.document.location.host.replace(/:.*/, ''),
     client = new Colyseus.Client(location.protocol.replace("http", "ws") + host + (location.port ? ':'+location.port : '')),
     room = client.join("tetris");
 
-document.addEventListener('keydown', system.input); // Motore Input
-setInterval(function(){ system.output.processFrame(); }, 40);	// Motore grafico
+
 
 
 room.onJoin.add(function () {
@@ -370,31 +316,31 @@ room.onJoin.add(function () {
 
 });
 
-room.onStateChange.addOnce(function(state) { });
+room.onStateChange.addOnce((state) => { });
 // new room state
-room.onStateChange.add(function(state) { });
+room.onStateChange.add((state) => { });
 // listen to patches coming from the server
-room.onMessage.add(function(data) { });
+room.onMessage.add((data) => { });
 
-
-room.listen("players/:uid", function(change)  {
+room.listen("players/:uid", (change) => {
     if (change.operation == "add"){
-        system.output.partita = change.value;
+        // system.output.partita = change.value;
     }
 });
-room.listen("players/:uid/gameOver", function(change)  {
+room.listen("players/:uid/gameOver", (change) => {
     if ((change.operation == "replace") && (change.value == true)){
         room.send({stop: true});
-        system.output.gameState = "gameover";
+        // system.output.gameState = "gameover";
     }
 });
-room.listen("players/:uid/schermata/:i/:j", function(change)  {
+
+room.listen("players/:uid/schermata/:i/:j", (change) => {
     if (change.operation == "replace"){
-        system.output.partita.schermata[change.path["i"]][change.path["j"]] = change.value;
+        // system.output.partita.schermata[change.path["i"]][change.path["j"]] = change.value;
     }
 });
 room.listen("players/:uid:/tetramino/:t", function(change)  {
     if (change.operation == "replace"){
-        system.output.partita.tetramino[change.path["t"]] = change.value;
+        // system.output.partita.tetramino[change.path["t"]] = change.value;
     }
 });
